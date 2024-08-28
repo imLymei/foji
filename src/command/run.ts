1103d99dfcd096673275cfbff6d4444cada17b08
Felipe Cardoso <lymeicontato@gmail.com>
2024-08-28 14:48:51 -0300

    feat: add command without `scope`

--- src/command/run.ts
+++ src/command/run.ts
@@ -13,7 +13,7 @@ const runCommand = new Command('run')
   .option('-d, --debug', 'Enable debugging features', false)
   .addHelpText('after', '\n* You can use "." as the run command alias *')
   .addHelpText('after', '\n* You can use "_" to skip a argument *')
-  .action(async (scope: string, command: string, args: string[]) => {
+  .action(async (scope?: string, command?: string, args?: string[]) => {
     const config = getConfig();
 
     if (!scope) {
@@ -34,6 +34,15 @@ const runCommand = new Command('run')
     if (!scopeObject) {
       console.error(`scope "${scope}" not found`);
       process.exitCode = 1;
+      return;
+    }
+
+    if (typeof scopeObject === 'string') {
+      runUserCommand(
+        scopeObject,
+        [command, ...args],
+        runCommand.getOptionValue('debug')
+      );
       return;
     }
 
