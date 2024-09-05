5f68e41ff978e31a8188ea4d081a66717c18a6ff
Felipe Cardoso <lymeicontato@gmail.com>
2024-09-05 17:49:20 -0300

    refactor: remove command scopes

--- src/command/config/addCommand.ts
+++ src/command/config/addCommand.ts
@@ -1,19 +1,13 @@
 import { Command } from 'commander';
-import { Config, updateConfig } from '../../lib/utils';
+import { addConfigCommand } from '../../lib/utils';
 
 const configAddCommand = new Command('add')
   .alias('a')
   .description('Add a new command to foji')
-  .argument('scope', 'Scope of the command')
   .argument('name', 'Command name')
-  .argument('[command]', 'Command to be added')
-  .action((scope: string, name: string, command?: string) => {
-    const newConfig: Config = {};
-
-    if (command) newConfig[scope] = { [name]: command };
-    else newConfig[scope] = name;
-
-    updateConfig(newConfig);
+  .argument('command', 'Command to be added')
+  .action((name: string, command: string) => {
+    addConfigCommand(name, command);
   });
 
 export default configAddCommand;
