51df7e8a4a8e2a12fcf3f53fc50612876ae9f41f
Felipe Cardoso <lymeicontato@gmail.com>
2024-09-06 10:35:57 -0300

    feat: make run command default foji action

--- src/command/index.ts
+++ src/command/index.ts
@@ -3,12 +3,35 @@ import { PROGRAM_DESCRIPTION, PROGRAM_NAME, PROGRAM_VERSION } from '../config';
 
 import configAddCommand from './config/addCommand';
 import configRemoveCommand from './config/removeCommand';
-import runCommand from './run';
 import openConfig from './config/openConfig';
 import configUpload from './config/configUpload';
 import configDownload from './config/configDownload';
+import { getConfig, logList, runUserCommand } from '../lib/utils';
 
-const program = new Command();
+const program = new Command()
+  .argument('[command]', 'Command that you want to run')
+  .argument('[args...]', 'Arguments for the command')
+  .option('-d, --debug', 'Enable debugging features', false)
+  .addHelpText('after', '\n* You can use "_" to skip a argument *')
+  .action(async (commandName?: string, args?: string[]) => {
+    if (!commandName) program.help();
+
+    const configCommands = getConfig().commands;
+
+    if (!commandName) {
+      logList('Commands', configCommands);
+      process.exit(0);
+    }
+
+    const command = configCommands[commandName];
+
+    if (!command) {
+      console.error(`command "${commandName}" not found`);
+      process.exit(1);
+    }
+
+    runUserCommand(command, args, program.getOptionValue('debug'));
+  });
 
 program
   .name(PROGRAM_NAME)
@@ -20,7 +43,5 @@ program.addCommand(configRemoveCommand);
 program.addCommand(openConfig);
 program.addCommand(configUpload);
 program.addCommand(configDownload);
-
-program.addCommand(runCommand);
 
 export default program;
